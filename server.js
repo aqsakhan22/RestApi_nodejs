9c7eb571481883ec3e083502ee45c4cdc3051141
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-02-20 15:46:21 +0500

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app =require('./app');
 const dotenv=require('dotenv');
 dotenv.config();
-const port=80;
+const port=3024;
 const server =http.createServer(app);
 server.listen(port,() => console.log(`server is running on ${port}`));
 
