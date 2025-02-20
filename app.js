32461ea13ce08535d0f1e9145afce47277a185ef
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-02-20 11:08:22 +0500

    first

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ const testRoute=require('./routes/test');
 
 app.use(bodyParser.json()); // Middleware to parse JSON
 //localhost:3000/uploads/1739340075259.png with get access
+app.get('/',function(req,res){
+    console.log("You accessing node jsRest Api");
+    res.send("Welcome to Node js Project");
+
+});
 app.use('/uploads',express.static('uploads'));
 app.use("/posts",postRoute);
 app.use("/user",userRoute);
 app.use("/association",testRoute);
+
 //localhost:3000/images/upload
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFxc2ExQGdtYWlsLmNvbSIsInVzZXJJZCI6MywiaWF0IjoxNzM5MzM5NDIzfQ.kPaO-N7uMvgLo2rw_dUCcZ6KG-g8Ku6sqKUaidaZjH8
 app.use('/images',imageRoute); //app.use is use for middleware 
--- server.js
+++ server.js
@@ -7,3 +7,4 @@ const server =http.createServer(app);
 server.listen(port,() => console.log(`server is running on ${port} ${process.env.PORT} `));
 
 
+
