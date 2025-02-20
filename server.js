db0a59c359c4e76fd39318b7a10cea214c766c5d
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-02-20 12:49:58 +0500

    port 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app =require('./app');
 const dotenv=require('dotenv');
 dotenv.config();
-const port=3040;
+const port=80;
 const server =http.createServer(app);
 server.listen(port,() => console.log(`server is running on ${port}`));
 
