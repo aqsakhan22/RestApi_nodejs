f4988302134e031e57ec295145cbef3f1825ba41
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-02-24 13:06:00 +0500

    mydb is a database name in rds instance

--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+
 const sequelize = new Sequelize({
-  database: 'db', // Replace with your RDS database name
-  username: 'admin',      // Replace with your RDS master username
-  password: 'aqsakhan1234',      // Replace with your RDS master password
-  host: 'db.clzrdwstoy7i.ap-south-1.rds.amazonaws.com',      // Replace with your RDS endpoint
+  database: process.env.DB_NAME, // Replace with your RDS database name
+  username: process.env.DB_USER,      // Replace with your RDS master username
+  password: process.env.DB_PASSWORD,      // Replace with your RDS master password
+  host: process.env.DB_HOST,      // Replace with your RDS endpoint
   port: 3306,                     // Default MySQL port
   dialect: 'mysql',                // Specify the dialect (MySQL)
-  dialectModule: require('mysql2'), // Use mysql2 as the dialect module
+  dialectModule: require('mysql2'),
+  dialectOptions: { //< Add this
+    ssl: {
+       require: true,
+       rejectUnauthorized: false
+    }
+ }, // Use mysql2 as the dialect module
   logging: console.log,            // Enable logging (optional)
   pool: {
     max: 5,
--- testConnection.js
+++ testConnection.js
@@ -1,11 +1,14 @@
 const sequelize = require('./config/database');
 
 async function testConnection() {
+  console.log("testConnection");
   try {
     await sequelize.authenticate();
     console.log('Connection to RDS MySQL database has been established successfully.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.log("**********");
+    console.log(error);
+    //console.error('Unable to connect to the database:', error);
   }
 }
 
