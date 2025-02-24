50f1714b5ddd85b97fd3d90c1de93cfd72bd6ad7
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-02-24 13:33:39 +0500

    sync file

--- sync.js
+++ sync.js
@@ -2,7 +2,7 @@ const sequelize = require('./config/database'); // Import the Sequelize instance
 //const User = require('./models/User'); // Import the User model
 
 // Synchronize all models with the database
-sequelize.sync({ force: true }) // `force: true` will drop the table if it already exists
+sequelize.sync({ alter : true }) // `force: true` will drop the table if it already exists
   .then(() => {
     console.log('All tables created successfully.');
   })
