15cda1b8f14ddb777635009e5947c41c06764894
aqsakhan22 <aqsaofficialdev@gmail.com>
2025-03-06 14:35:06 +0500

    category Api Added

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -23,12 +23,51 @@ models.Category.findAll().then(
 });
  
 }
+function save(req,res){
+    //console.log(`req is ${req.body}`);
+const category = {
+    name:req.body.title,
+    
+};
+models.Category.findOne({where:{name:req.body.title}}).then(result =>{
+     if(result){
+        res.status(201).json({
+            message:"Category Already Found No Need To Add"
+        });
 
+     }
+     else{
+     
+        models.Category.create(category).then(result => {
+            // console.log('post creation started');
+              res.status(201).json({
+                 message:"Category Added Succesfully",
+                 category:result
+              });
+         }).catch(error => {
+           //  console.log('post error');
+             res.status(500).json({
+                 message:"Something went wrong please try again",
+                 error:error
+              });
+             
+             
+         });
+
+
+     }
+
+});
+
+
+
+};
 
 
 
 module.exports={
 
-    index:index
+    index:index,
+    save:save
 
 }
--- routes/category.js
+++ routes/category.js
@@ -2,4 +2,5 @@ const express=require('express');
 const categoryController = require('../controllers/category.controller');
 const router=express.Router();
 router.get("/",categoryController.index);
+router.post("/addCategory",categoryController.save);
 module.exports=router;
